a5cf3acb590bc4e4cf995f0aaeddb818371af0a3
ian brain <ianbrain080@gmail.com>
2023-05-27 08:46:37 -0400

    Imp: Added deck model and table to decks-list.

--- src/app/components/decks/decks-list/decks-list.component.ts
+++ src/app/components/decks/decks-list/decks-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Deck } from 'src/app/models/deck.model';
 
 @Component({
   selector: 'app-decks-list',
   templateUrl: './decks-list.component.html',
   styleUrls: ['./decks-list.component.css']
 })
-export class DecksListComponent {
+export class DecksListComponent implements OnInit {
 
+  decks: Deck[] = [
+    {
+      id: 1,
+      title: 'deck 1',
+      description: 'description 1'
+    },
+    {
+      id: 2,
+      title: 'deck 2',
+      description: 'description 2'
+    }
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.decks.push();
+  }
 }
