10163c30fd818ae3ac8feef9f599449359ea2de1
ian brain <ianbrain080@gmail.com>
2023-05-27 08:37:31 -0400

    Imp: Added routing to the home and decks page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DecksListComponent } from './components/decks/decks-list/decks-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DecksListComponent
+  },
+  {
+    path: 'decks',
+    component: DecksListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
